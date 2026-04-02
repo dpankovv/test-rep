3fd2cc354ceaaf0dd3a110641132d9ad4dee9442
dpn <sww7839@gmail.com>
2026-04-02 19:37:34 +0300

    was

--- hoh.py
+++ hoh.py
@@ -1,1 +1,11 @@
-print("Hello World")
+a = int(input("Whats x?"))
+b = int(input("Whats y?"))
+if a > b:
+    print("a is greater than b")
+    if a < b:
+        print("a is less than b")
+        if a == b:
+            print("a is equal to b")
+            if a != b:
+                print("a is not equal to b")
+
